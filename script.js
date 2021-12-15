0bd98d0ce605b778b1f3c65bf1e206ab6e560e4b
Francisco Dias <39599289+FranciscoDias87@users.noreply.github.com>
2021-12-15 14:31:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-let outputSreen = document.getElementById('putp')
+let outputSreen = document.getElementById('outputSreen');
+
+funcion display(num){
+outputSreen.value += num;
+}
+
+function calculate(){
+try{
+outputSreen.value = eval(outputSreen.value);
+}
+catch(){
+alert("Entrada Inválida");
+}
+}
+
+function Clear(){
+outputSreen.value = "";
+}
+
+function Del(){
+outputSreen.value = outputSreen.value.slice(0,-1);
+}
